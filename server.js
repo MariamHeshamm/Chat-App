0b130d9c6e5ec09350c802f32131f70288f7652c
Mariam Hesham <mariamhesham503@gmail.com>
2023-04-01 16:00:45 +0200

    server Updated

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
 
-var io = require('socket.io')(3000, {cors: {origin: "*"}});
+//var io = require('socket.io')(3000, {cors: {origin: "*"}});
+const express = require('express');
+const http = require('http');
+var app = express();
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+ const path = require("path");
+// app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+
+//app.use(express.static(__dirname+"/public"));
+
+
+app.get('/', (req, res) => {
+//   res.sendFile(__dirname + 'home.ejs');
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+app.get('/client.js', (req, res) => {
+    //   res.sendFile(__dirname + 'home.ejs');
+      res.sendFile(path.join(__dirname, 'client.js'));
+    });
 io.on('connection',(socket)=>
 {
     console.log('a user is connected');
@@ -12,3 +33,6 @@ console.log('user disconnected');
     })
 
 })
+server.listen(3000,'0.0.0.0', () => {
+    console.log("http://localhost:"+ 3000);
+  });
